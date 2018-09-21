e815c358f368e39fb61e7f57ab1cc7c6bab65e0d
Berenisse Rios <berenisser@gmail.com>
2018-09-20 23:31:37 -0300

    push to git repo

--- src/app/pages/item/item.component.ts
+++ src/app/pages/item/item.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductosService } from '../../services/productos.service';
+import { Subscriber } from 'rxjs';
+import { ProductoDescripcion } from '../../interfaces/producto-descripcion.interface';
 
 @Component({
   selector: 'app-item',
@@ -6,10 +10,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./item.component.css']
 })
 export class ItemComponent implements OnInit {
+  producto: ProductoDescripcion;
+  id: string;
 
-  constructor() { }
+  constructor( private route: ActivatedRoute,
+              public productosService: ProductosService ) { }
 
   ngOnInit() {
+    this.route.params
+      .subscribe( parametros => {
+        console.log(parametros['id']);
+
+        this.productosService.getProducto(parametros['id'])
+          .subscribe( (producto: ProductoDescripcion) => {
+            this.id = parametros['id'];
+            this.producto = producto;
+          });
+      });
   }
 
 }
--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -8,17 +8,54 @@ import { Producto } from '../interfaces/producto.interface';
 export class ProductosService {
   cargando = true;
   productos: Producto[] = [];
+  productosFiltrado: Producto[] = [];
 
   constructor( private http: HttpClient ) {
     this.cargarProductos();
   }
 
   private cargarProductos() {
-    this.http.get('https://devestudio-88afc.firebaseio.com/productos_idx.json')
-      .subscribe( (resp: Producto[]) => {
-        console.log(resp);
-        this.productos = resp;
-        this.cargando = false;
+
+    return new Promise( (resolve, reject) => {
+      this.http.get('https://devestudio-88afc.firebaseio.com/productos_idx.json')
+        .subscribe( (resp: Producto[]) => {
+          this.productos = resp;
+          this.cargando = false;
+          resolve();
       });
+    });
+  }
+
+  getProducto( id: string) {
+    return this.http.get(`https://devestudio-88afc.firebaseio.com/productos/${ id }.json`);
+  }
+
+  buscarProducto( termino: string ) {
+
+    if ( this.productos.length === 0) {
+      // cargar productos
+      this.cargarProductos().then( () => {
+         // ejecutar despues de tener los productos
+        // aplicar filtro
+        this.filtrarProductos( termino );
+      });
+    } else {
+      // aplicar filtro
+      this.filtrarProductos( termino );
+    }
+  }
+
+  private filtrarProductos( termino: string ) {
+
+    this.productos.forEach( prod => {
+      if ( prod.categoria.indexOf( termino ) >= 0 ) {
+
+      }
+    });
+
+    this.productosFiltrado = this.productos.filter( producto => {
+      return true;
+    });
+
   }
 }
